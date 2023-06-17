bcc4e3b4c5a291a2c23c521c929fed8dd46bfc42
Bruno Pires <brunopires.ctto@gmail.com>
2023-06-17 15:23:00 -0300

    Imprementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,10 +16,62 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position position) {  						  	 // DEFINIR COMO O "REI" DEVE SE MOVIMENTAR
+		ChessPiece p = (ChessPiece)getBoard().piece(position); 				 // 1- PEGA A PEÇA P QUE ESTÁ NESTA POSIÇÃO
+		return p == null || p.getColor() != getColor();						 // 2- VERIFICA SE A PEÇA É NULA OU TBM SE É ADVERSARIA, NESSES CASO PODE MOVER
+		
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// ABOVE (ACIMA)
+		p.setValues(position.getRow()-1, position.getColumn()); 			// VAI ANALISAR A POSIÇÃO ACIMA DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		
+		// BELOW (ABAIXO)
+		p.setValues(position.getRow()+1, position.getColumn()); 			// VAI ANALISAR A POSIÇÃO ABAIXO DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		
+		// LEFT (ESQUERDA)
+		p.setValues(position.getRow(), position.getColumn()-1); 			// VAI ANALISAR A POSIÇÃO A ESQUERDA DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		// RIGHT (DIREITA)
+		p.setValues(position.getRow(), position.getColumn()+1); 			// VAI ANALISAR A POSIÇÃO A DIREITA DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		// NW (NOROESTE)
+		p.setValues(position.getRow()-1, position.getColumn()-1); 			// VAI ANALISAR A POSIÇÃO A NOROESTE DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		// NE (NORDESTE)
+		p.setValues(position.getRow()-1, position.getColumn()+1); 			// VAI ANALISAR A POSIÇÃO A NORDESTE DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		// SW (SUDOESTE)
+		p.setValues(position.getRow()+1, position.getColumn()-1); 			// VAI ANALISAR A POSIÇÃO A SUDOESTE DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		// SE (SUDESTE)
+		p.setValues(position.getRow()+1, position.getColumn()+1); 			// VAI ANALISAR A POSIÇÃO A SUDESTE DA PEÇA 
+		if (getBoard().positionExists(p) && canMove(p)) {					// 1- SE ESSA PEÇA PODE MOVER PRA POSIÇÃO P
+			mat [p.getRow()][p.getColumn()] = true;							// 2- ENTÃO ESSA POSIÇÃO P-ROW E P-COLUM É VERDADEIRO
+		}
+		
 		return mat;
 	}
 }
